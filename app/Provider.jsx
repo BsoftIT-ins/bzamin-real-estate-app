d65ea8e712bc291a3bffc39b4f6353b0e03106be
Azizul Islam <bsoftitinfo@gmail.com>
2024-05-08 16:17:42 +0600

    update

---
+++ app/Provider.jsx
@@ -0,0 +1,15 @@
+
+import Header from './_components/Header'
+
+const Provider = ({children}) => {
+  return (
+    <div>
+    <Header />
+        <div className='mt-20'>
+            {children}  
+        </div>
+    </div>
+  )
+}
+
+export default Provider
---
+++ app/_components/Header.jsx
@@ -0,0 +1,35 @@
+"use client"
+import { Button } from '@/components/ui/button'
+import { PlusIcon } from '@radix-ui/react-icons'
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { useEffect } from 'react'
+import React from 'react'
+
+const Header = () => {
+    const path=usePathname();
+
+    useEffect(()=>{
+       
+    },[])  
+  return (
+    <div className='p-6 px-10 flex justify-between shadow fixed top-0 w-full z-10 bg-white'>
+        <div className='flex gap-12 items-center'>
+        <Image src={'/logo.svg'} alt='logo' width={100} height={100} className='mt-2 mx-5'  />
+            <ul className='hidden md:flex gap-10'>
+               <Link href={'/'}> <li className={`'hover:text-primary font-medium text-sm cursor-pointer' ${path=='/'&&'text-primary'}`}>For Rent</li>
+               </Link>
+                <li className='hover:text-primary font-medium text-sm cursor-pointer '>For Sell</li>
+                <li className='hover:text-primary font-medium text-sm cursor-pointer '>Agent Finder</li>
+            </ul>
+        </div>
+        <div className='flex gap-2'>
+            <Button className='flex gap-2'><PlusIcon className='h-5 w-5'/>Post your Ad</Button>
+            <Button variant='outline'>Login</Button>
+        </div>
+    </div>
+  )
+}
+
+export default Header

1e00e47fa6a1135333e9c5517811b2f17ca59444
Azizul Islam <bsoftitinfo@gmail.com>
2024-05-09 19:46:31 +0600

    updted

--- app/(routes)/add-new-listing/page.jsx
+++ app/(routes)/add-new-listing/page.jsx
@@ -1,16 +1,55 @@
+"use client"
 import GoogleAddressSearch from '@/app/_components/GoogleAddressSearch';
-import React from 'react'
+import { Button } from '@/components/ui/button';
+import { supabase } from '@/utils/supabase/client';
+import { useUser } from '@clerk/nextjs';
+import React, { useState } from 'react'
 
 
 
 const AddNewListing = () => {
+  const [selectedAddress, setSelectedAddress] = useState();
+  const [coordinates, setCoordinates] = useState();
+
+  const {user}=useUser();
+
+  const nextHandler=async()=>{
+    console.log(selectedAddress, coordinates);
+    
+    const { data, error } = await supabase
+    .from('bzameen_database')
+    .insert([
+      {address: selectedAddress, 
+        coordinates: coordinates, 
+        createdBy:user?.primaryEmailAddress.emailAddress
+      },
+    ])
+    .select();
+    
+    if(data){
+      console.log("New data Added,", data);
+    }
+    if(error){
+      console.log("Error");
+    }
+
+  }
   return (
+    <div className='mt-10 md:mx-56 lg:mx-80'>
     <div className='p-10 flex flex-col gap-5 items-center justify-center'>
         <h2 className='font-bold text-2xl'>Add New Listing</h2>
-        <div>
+        <div className='p-5 w-full rounded-lg border shadow-md flex flex-col gap-5'>
             <h2 className='text-grey-500'>Eneter Address which you want to list</h2>
-            <GoogleAddressSearch/>
+            <GoogleAddressSearch
+            selectedAddress={(value)=>setSelectedAddress(value)}
+            setCoordinates={(value)=>setCoordinates(value)}
+            />
+            <Button
+            disabled={!selectedAddress || !coordinates}
+            onClick={nextHandler}
+            >Next</Button>
         </div>
+    </div>
     </div>
   )
 }
--- app/_components/GoogleAddressSearch.jsx
+++ app/_components/GoogleAddressSearch.jsx
@@ -1,22 +1,29 @@
 "use client"
 
-import { MapPin } from 'lucide'
-import React from 'react'
-import GooglePlacesAutocomplete from 'react-google-places-autocomplete'
 
-const GoogleAddressSearch=()=> {
+import { MapPinIcon } from 'lucide-react'
+import React from 'react'
+import GooglePlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-google-places-autocomplete'
+
+function GoogleAddressSearch({selectedAddress, setCoordinates}) {
   return (
-    <div className='flex gap-2 items-center w-full'>  
-    <MapPin className='h-10 w-10 p-2 rounded-full text-primary bg-purple-200' />    
+    <div className='flex items-center w-full'>  
+    <MapPinIcon className='h-10 w-10 p-2 rounded-l-lg text-primary bg-purple-200'/>
     <GooglePlacesAutocomplete  
     apiKey={process.env.NEXT_PUBLIC_GOOGLE_PLACE_API_KEY}
     selectProps={{
-        placeholder: 'Search Property Address',
-        isClearable:true,
-        className:'w-full',
-        onChange:(place)=>{
-            console.log(place);
-        }
+      placeholder:'Enter Property Address',
+      isClearable:true,
+      className:'w-full',
+      onChange:(place)=>{
+        console.log(place);
+        selectedAddress(place);
+         geocodeByAddress(place.label)
+        .then(result=>getLatLng(result[0]))
+        .then(({lat,lng})=>{
+          setCoordinates({lat,lng})
+        })
+      }
     }}
   />
   </div>
